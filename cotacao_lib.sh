291713d778915fcf896f104db1c9d430eb67e592
Paulo Ortolan <paulo.ortolan@gmail.com>
2019-05-20 16:27:45 +0100

    Implementacao inicial da biblioteca de cotações

--- cotacao_lib.sh
+++ cotacao_lib.sh
@@ -1,13 +1,32 @@
 #!/usr/bin/env sh
 
 function obterCotaca() {
-  
+  MOEDA=$1
+  echo $(curl $CURL_PARAMETERS $SOURCE_DATA$MOEDA$SOURCE_COMMON_SUFFIX)
 }
 
 function oterDataDeTimestamp() {
   echo $(date -d @$1)
 }
 
-function imprimirDadosCotacao() {
+function obterCampoDoJSON() {
+  MOEDA=$1
+  CAMPO=$2
+  echo $(jq $JQ_OUTPUT .$MOEDA$CAMPO)
+}
 
+function imprimirDadosCotacao() {
+  echo $SAIDA_CONSOLE <<EOF
+Valor moeda:
+
+$COTACAO_NAME - ($COTACAO_CODE)
+----------------------------------------------
+Máximo      $COTACAO_MAXIMO
+Mínimo      $COTACAO_MINIMO
+Compra      $COTACAO_COMPRA
+Venda       $COTACAO_VENDA
+
+Atualizado em: $(obterDataDeTimestamp $COTACAO_TIMESTAMP)
+
+EOF
 }
